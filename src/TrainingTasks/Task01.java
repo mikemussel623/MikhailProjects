c0eb1e87e538939661c3ff0385d20a689097207d
mikemussel623 <mikemussel623@gmail.com>
2022-06-24 03:42:08 +0300

    just for fun

---
+++ src/TrainingTasks/Task01.java
@@ -0,0 +1,57 @@
+package TrainingTasks;
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+
+        System.out.println("Task #1");
+        System.out.println("Hello");
+        System.out.println("John Toki");
+
+        System.out.println("Task #2");
+        int a = 74;
+        int b = 36;
+        System.out.println(a + b);
+
+        System.out.println("Task #3");
+        int c = 50;
+        System.out.println(c / 3);
+
+        System.out.println("Task #4");
+        System.out.println((-5 + 8 * 6));
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+
+/*        System.out.println("Task #5");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input first number: ");
+        int number1 = input.nextInt();
+        System.out.println("Input second number: ");
+        int number2 = input.nextInt();
+
+        System.out.println(number1 * number2);
+*/
+        System.out.println("Task #6");
+
+        int z = 125;
+        int x = 24;
+
+        System.out.println(z + x);
+        System.out.println(z - x);
+        System.out.println(z * x);
+        System.out.println(z / x);
+        System.out.println(z % x);
+
+        System.out.println("Task #7");
+
+
+
+
+
+
+
+    }
+}
